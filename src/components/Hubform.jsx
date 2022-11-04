98021a81ea2799f0c02eb2bc37875be0b019ad63
flapon22 <flapon22@gmail.com>
2022-11-04 15:12:09 -0600

    landing page smarteam

---
+++ src/components/Hubform.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import styles from "../form-style";
+
+const Hubform = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    const style = document.createElement("style");
+    const mainNode = document.createElement("hubspotForm");
+    let frameListener;
+
+    script.src = "https://js.hsforms.net/forms/v2.js";
+
+    if (!mainNode.hasChildNodes()) {
+      document.body.appendChild(script);
+    }
+
+    const frameLoaded = () => {
+      var frame = document.querySelector("iframe");
+      if (frame != null) {
+        var frmHead = frame.contentDocument.querySelector("head");
+
+        if (frmHead != null) {
+          clearInterval(frameListener); // stop the listener
+          frmHead.appendChild(style);
+          style.appendChild(document.createTextNode(styles));
+        }
+      }
+    };
+
+    frameListener = () => setInterval(frameLoaded(), 50);
+
+    window.addEventListener("load", () => {
+      let ready = null;
+      if (window.hbspt) {
+        ready = window.hbspt.forms.create({
+          region: "na1",
+          portalId: "6553628",
+          formId: "7a53f2e8-6e1f-4235-93d4-17f48b91a62e",
+          target: "#hubspotForm",
+        });
+      }
+
+      ready.onReady(() => {
+        frameListener();
+      });
+    });
+  }, []);
+
+  return <div id="hubspotForm"></div>;
+};
+
+export default Hubform;
